c809754cb16d4c6ad133833bc9f63c44288fbdb7
pboy <pboyfunclub@gmail.com>
2018-07-24 15:47:55 +0200

    fixed foo read

--- as07/main.c
+++ as07/main.c
@@ -9,7 +9,7 @@
 #define LEN 8
 #define USERNAME "ttshivhu"
 
-char kbuff[LEN];
+char kbuff[LEN], foobuff[PAGE_SIZE];
 struct dentry *root;
 int ret;
 
@@ -46,6 +46,32 @@ static ssize_t ft_write(struct file *f, const char __user *buf, size_t len, loff
 	return (ret);
 }
 
+static ssize_t foor(struct file *f, char __user *buffer, size_t length, loff_t *offset)
+{
+	char *read_from = foobuff + *offset;
+	size_t read_num = length < (PAGE_SIZE - *offset) ? length : (PAGE_SIZE - *offset);
+
+	if (read_num == 0)
+		return (0);
+	ret = copy_to_user(buffer, read_from, read_num);
+	if (ret == read_num) {
+		ret = -EIO;
+	} else {
+		*offset = PAGE_SIZE - ret;
+		ret = read_num - ret;
+	}
+	return ret;
+}
+
+/*
+ * TODO: Implement real write mode that supports, appending.
+ */
+
+static ssize_t foow(struct file *f, const char __user *buf, size_t len, loff_t *offset)
+{
+	return (0);
+}
+
 static struct file_operations idfops = {
   .owner = THIS_MODULE,
   .read = ft_read,
@@ -54,20 +80,22 @@ static struct file_operations idfops = {
 
 static struct file_operations foofops = {
   .owner = THIS_MODULE,
-  .read = ft_read,
-  .write = ft_write,
+  .read = foor,
+  .write = foow,
 };
 
 static int __init entry_point(void)
 {
+	printk(KERN_INFO "Hello world!\n");
 	root = debugfs_create_dir("fortytwo", NULL);
 	if (!root || root == (void *)-ENODEV)
 		return (-1);
-	debugfs_create_file("id", 0666, root, NULL, &idfops);
-	debugfs_create_ulong("jiffies", 0444, root,
-			   (long unsigned int *)&jiffies);
-	debugfs_create_file("foo", 0644, root, NULL, &idfops);
-	printk(KERN_INFO "Hello world!\n");
+	if (!(debugfs_create_file("id", 0666, root, NULL, &idfops) &&
+			debugfs_create_ulong("jiffies", 0444, root,
+			   (long unsigned int *)&jiffies) &&
+			debugfs_create_file("foo", 0644, root, NULL,
+				&foofops)))
+		return (-1);
 	return 0;
 }
 
